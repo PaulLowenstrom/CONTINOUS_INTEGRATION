e0e8f70f0263875ae2eefa310f733dbb81633402
Oliver Schwalbe Lehtihet <oliverlehtihet@gmail.com>
2020-02-07 02:30:32 +0100

    Remove redundant imports from two files. Fix #28

--- src/main/java/com/group21/continous_integration/BuildResult.java
+++ src/main/java/com/group21/continous_integration/BuildResult.java
@@ -1,13 +1,5 @@
 package com.group21.continous_integration;
 
-import org.apache.maven.shared.invoker.*;
-import org.eclipse.jgit.api.*;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import java.io.File;
-import java.util.Arrays;
-import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.nio.file.FileSystems;
 import java.io.Serializable;
 
 
