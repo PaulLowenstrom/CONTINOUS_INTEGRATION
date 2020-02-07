cbd65ee1ecc826a078a1fbd507f720a7a6a858b5
PaulLowenstrom <46889009+PaulLowenstrom@users.noreply.github.com>
2020-02-07 11:49:39 +0100

    Add feature (#27)
    -Add send email about build to user
    
    see issue  #26

---
+++ src/main/java/com/group21/continous_integration/EmailService.java
@@ -0,0 +1,66 @@
+package com.group21.continous_integration;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.Properties;
+
+public class EmailService {
+
+    private static String host = "smtp-mail.outlook.com";
+    private static int port = 587;
+    private static String username = "CISendBuildReport@outlook.com";
+    private static String password = "byggarebob123";
+
+    public static void SendBuildSuccesfull(String sendTo, String branch){
+      sendMail(sendTo, branch, "Build Succesfull");
+    }
+
+    public static void SendBuildFailure(String sendTo, String branch){
+      sendMail(sendTo, branch, "Build Failed");
+    }
+
+    public static void sendMail(String sendTo, String branch, String subject) {
+      System.out.println("Sending mail");
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", host);
+        prop.put("mail.smtp.port", port);
+        prop.put("mail.smtp.ssl.trust", host);
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("CISendBuildReport@outlook.com"));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(sendTo));
+                message.setSubject(subject);
+
+                String msg =  "Branch: " + branch + " failed to build. Please check.";
+
+                MimeBodyPart mimeBodyPart = new MimeBodyPart();
+                mimeBodyPart.setContent(msg, "text/html");
+
+                Multipart multipart = new MimeMultipart();
+                multipart.addBodyPart(mimeBodyPart);
+
+                message.setContent(multipart);
+
+                Transport.send(message);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
